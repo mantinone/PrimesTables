bc851654763d635dfd8ca64029607dff43f051ac
Max Antinone <mantinone@gmail.com>
2018-02-07 17:24:46 -0800

    New function, new failing tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,32 @@
 const chai = require('chai')
 const expect = chai.expect
-const {nPrimes} = require('../src/index.js')
+const {nPrimes, checkPrime} = require('../src/index.js')
 
-describe('The nPrimes function', ()=>{
+describe('The checkPrime function', () => {
+  it('Should return true for prime numbers', () => {
+    expect(checkPrime(2)).to.be.true
+    expect(checkPrime(5)).to.be.true
+    expect(checkPrime(11)).to.be.true
+    expect(checkPrime(127)).to.be.true
+    expect(checkPrime(199)).to.be.true
+  })
+
+  it('Should return false for non prime integers', () => {
+    expect(checkPrime(4)).to.be.false
+    expect(checkPrime(27)).to.be.false
+    expect(checkPrime(100)).to.be.false
+    expect(checkPrime(5.5)).to.be.false
+    expect(checkPrime(2.5)).to.be.false
+  })
+
+  it('Should return false for negatives and for bad input', () => {
+    expect(checkPrime(-2)).to.be.false
+    expect(checkPrime(-7)).to.be.false
+    expect(checkPrime("fish")).to.be.false
+  })
+})
+
+describe('The nPrimes function', () => {
   it('Should take return an array of the first N primes, where N is a positive integer', () => {
     expect(nPrimes(1)).to.eql([2])
     expect(nPrimes(5)).to.eql([2,3,5,7,11])
